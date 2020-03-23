7a3013a8b3665bde62550ebc8e5360c633233b2e
OliviaL18 <oliviatloos@gmail.com>
2020-03-23 18:11:04 -0400

    changed menu to a switch statement

--- script.js
+++ script.js
@@ -15,41 +15,39 @@ connection.connect(function(err) {
     startApp();
 });
 
-let mainAction;
+const menuPrompt = [{
+    type: 'list',
+    message: "What do you want to do?",
+    choices: [
+        "View All Employees", 
+        "View Employees by Department",
+        "Add Employee",
+        "Remove Employee" ,
+        "Update Employee", 
+        "View All Departments",
+        "View All Roles", 
+        "Update Role"               
+    ],
+    name: "mainAction"
+}];
 
 function startApp() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                message: "What do you want to do?",
-                choices: [
-                    "View All Employees", 
-                    "View Employees by Department",
-                    "Add Employee",
-                    "Remove Employee" ,
-                    "Update Employee", 
-                    "View All Departments",
-                    "View All Roles", 
-                    "Update Role"               
-                ],
-                name: "mainAction"
-            }
-        ])
-        .then(answers => {
-            mainAction = answers.mainAction;
-                console.log(`Select Action: ${mainAction}`);
-            function readEmployees() {
+    inquirer.prompt(menuPrompt).then(answers => {
+        switch (answers.mainAction){
+            case "View All Employees":
+                console.log(`Select Action
+                : ${answers.mainAction}`);
                 connection.query(
                     'SELECT e.employee_name, d.deptName, r.title, r.salary FROM employees e LEFT JOIN roles r ON r.id = e.role_id LEFT JOIN departments d ON d.id = r.department_id', 
-                    function(err, res) {
+                    function(err, res) { 
                         if (err) throw err;
                         console.log("------------ EMPLOYEES ------------");
                         console.table(res);
                     }
                 );
-            }
-            function readDepts() {
+                break;
+            case "View All Departments":
+                console.log(`Select Action: ${answers.mainAction}`);
                 connection.query(
                     'SELECT d.deptName FROM departments d', 
                     function(err, res) {
@@ -58,8 +56,9 @@ function startApp() {
                         console.table(res);
                     }
                 );
-            }
-            function readRoles() {
+                break;
+            case "View All Roles":
+                console.log(`Select Action: ${answers.mainAction}`);
                 connection.query(
                     'SELECT r.title, r.salary, d.deptName FROM roles r LEFT JOIN departments d ON d.id = r.department_id', 
                     function(err, res) {
@@ -68,13 +67,74 @@ function startApp() {
                         console.table(res);
                     }
                 );
-            }
-            if (mainAction == "View All Employees") {
-                readEmployees();
-            } else if (mainAction === "View All Departments") {
-                readDepts();
-            } else if (mainAction === "View All Roles") {
-                readRoles();
-            }
-        });
+                break;
+        }
+    });
 }
+
+
+
+
+       /*
+
+            function addEmployee(){
+                console.log("------------ Add Employee ------------");
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: "Employee Name?",
+                            name: "employeeName"
+                        },
+                        {
+                            type: "list",
+                            message: "Employee Role?",
+                            choices: [
+                                "Business Analyst",
+                                "Data Analyst",
+                                "Documentation Specialist",
+                                "Performance Analyst", 
+                                "Investment Analyst", 
+                                "Client Service Coordinator", 
+                                "Research Analyst", 
+                                "Research Associate", 
+                                "Compliance Manager", 
+                                "Compliance Specialist", 
+                                "Office Manager", 
+                                "Executive Assistant" 
+                            ],
+                            name: "employeeRole"
+                        }
+                    ])
+                    .then(answers => {
+                        let employeeName = answers.employeeName;
+                        let employeeRole = answers.employeeRole;
+                        //change this later
+                        let roles = [
+                            "Business Analyst",
+                            "Data Analyst",
+                            "Documentation Specialist",
+                            "Performance Analyst", 
+                            "Investment Analyst", 
+                            "Client Service Coordinator", 
+                            "Research Analyst", 
+                            "Research Associate", 
+                            "Compliance Manager", 
+                            "Compliance Specialist", 
+                            "Office Manager", 
+                            "Executive Assistant"
+                        ];
+                        let roleId;
+                        for (let i=0; i<roles.length; i++) {
+                            if (employeeRole == roles[i]) {
+                                roleId = roles[i].indexOf() ;
+                                console.log(roleId);
+                            }
+                        }
+                        console.log(employeeRole);
+                    //connection.query(
+                    });
+            }
+      */
+        
+
